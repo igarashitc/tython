6217b65899105c8e5a71aa5aed052c911f085915
igarashitc <igarashi.taichi@chiba-u.jp>
2021-08-29 14:14:34 +0900

    modify myplot.py

--- myplot.py
+++ myplot.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def movie(data,x,y,z,cmap,vmx,vmn,x1,z1,z2,dir):
+def movie_rz(data,x,y,z,cmap,vmx,vmn,x1,z1,z2,dir):
     nt = data.shape[0]
 
-    for l in range(0,nt):
+    for l in range(0,nt-1):
         plt.pcolormesh(x,z,data[l,:,0,:],cmap=cmap,vmax=vmx,vmin=vmn)
         plt.xlim(0,x1)
         plt.ylim(z1,z2)
