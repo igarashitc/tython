8ebf9116c225933a7eed874272d8f8332381e524
Taichi <igararashi.taichi@chiba-u.jp>
2023-07-25 15:30:08 +0900

    bug fix

--- myplot.py
+++ myplot.py
@@ -9,12 +9,12 @@ class plot_cyl:
         dr = abs(r[1]-r[0])*0.5e0
         dp = abs(p[1]-p[0])*0.5e0
 
-        tmp = p
+        tmp = p.copy()
         tmp[1:] = tmp[1:]+dp
-        R,P = np.meshgrid(r-0.5*dr,tmp)
-        X,Y = R*np.cos(P), R*np.sin(P)
+        Rad,Phi = np.meshgrid(r-0.5*dr,tmp)
+        X,Y = Rad*np.cos(Phi), Rad*np.sin(Phi)
 
-        plt.pcolormesh(X,Y,data,cmap=cmp,vmax=vmax,vmin=vmin,shading="gourand")
+        plt.pcolormesh(X,Y,data,cmap=cmap,vmax=vmax,vmin=vmin,shading="gourand")
         plt.gca().set_aspect("equal")
 
         return 
